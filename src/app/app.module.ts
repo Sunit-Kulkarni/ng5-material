af48d7d6f0db248a0788becdcfa0a0a71b7edbc9
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-03-19 18:11:34 -0400

    added form and methods with spinner to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
